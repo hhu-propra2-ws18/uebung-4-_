34f70a500960bc6219cb1c10d40cbf48671a7e66
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-11-15 15:48:12 +0100

    fertiges Projekt

--- src/main/java/de/hhu/propra/db/data/KundeRepository.java
+++ src/main/java/de/hhu/propra/db/data/KundeRepository.java
@@ -1,8 +1,10 @@
 package de.hhu.propra.db.data;
 
-import de.hhu.propra.db.entities.Kunde;
-import org.springframework.data.repository.CrudRepository;
+        import de.hhu.propra.db.entities.Kunde;
+        import org.springframework.data.repository.CrudRepository;
+
+        import java.util.List;
 
 public interface KundeRepository extends CrudRepository<Kunde, Long> {
-
+    List<Kunde> findAll();
 }
--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,22 @@
 package de.hhu.propra.db.entities;
 
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Auto{
+
+@Id
+@GeneratedValue
+private Long id;
+
+private String marke;
+
+private String farbe;
+
+private Integer hubraum;
 
 }
